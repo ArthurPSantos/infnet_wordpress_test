2d238feb23f3cf9d5706af12064b00c2682544b0
ArthurPSantos <34771184+ArthurPSantos@users.noreply.github.com>
2022-12-20 20:24:55 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,11 @@
             <select id="partidos-dropdown" name="partidos">
             </select>
         </div>
-        <button type="submit" onclick="javascript:enviar()">Pegar dados do Partido</button>
+        <button type="submit" onclick="javascript:pegar_parlamentares()">Pegar Parlamentares do Partido</button>
+        <div id='div-table'>
+            <table id="excelDataTable" border="1">
+            </table>
+        </div>
     </body>
 </html>
 
@@ -17,26 +21,23 @@ function pegar_partidos()
     
     let defaultOption = document.createElement('option');
     defaultOption.text = 'Escolha o partido';
-    
     dropdown.add(defaultOption);
     dropdown.selectedIndex = 0;
     
-    const url = 'https://dadosabertos.camara.leg.br/api/v2/partidos?ordem=ASC&ordenarPor=sigla';
-    
-    const request = new XMLHttpRequest();
-    request.open('GET', url, true);
+    var url = 'https://dadosabertos.camara.leg.br/api/v2/partidos?ordem=ASC&ordenarPor=sigla';
+    var request = new XMLHttpRequest();
+    request.open('GET', url, false);
     
     request.onload = function() {
       if (request.status === 200) {
-        const data = JSON.parse(request.responseText);
+        var data = JSON.parse(request.responseText);
         let option;
         for (let i = 0; i < data.dados.length; i++) {
           option = document.createElement('option');
           option.text = data.dados[i].sigla;
-          option.value = data.dados[i].id;
+          option.value = data.dados[i].uri;
           dropdown.add(option);
         }
-        console.log(data)
        } else {
         console.error('Erro ao tentar resgatar os dados de ' + url);
       }   
@@ -44,14 +45,43 @@ function pegar_partidos()
     
     request.onerror = function() {
       console.error('Erro ao tentar resgatar os dados de ' + url);
-};
+    };
 
-request.send();
+    request.send();
 }
 
-function enviar()
+function pegar_parlamentares()
 {
+    var e = document.getElementById("partidos-dropdown").value;
+    var urlt = e + '/membros';
+    var requestt = new XMLHttpRequest();
+    requestt.open('GET', urlt, true);
     
+    requestt.onload = function() {
+        if (requestt.status === 200) {
+            var datat = JSON.parse(requestt.responseText);
+            var keys = [];
+
+            document.write("<table border==\"1\"><tr>");
+            for (key in datat.dados[0]) {
+            	document.write('<td>' + key + '</td>');
+            }
+            document.write("</tr>");
+            for (var i = 0; i < datat.dados.length; i++) {
+            	document.write('<tr>');
+            	for (key in datat.dados[i]) {
+              	document.write('<td>' + datat.dados[i][key] + '</td>');
+              }
+            	document.write('</tr>');
+            }
+            document.write("</table>");
+
+        }
+        else {
+            console.error(requestt.status);
+        }
+    }
+    requestt.send();
 }
 
 function listener() {
